f48fb67e879ea42a0b0699c92b0f93fa9c26d1e3
K M Farhat Snigdah <farhatsnigdho1392@gmail.com>
2023-01-19 23:13:43 +0600

    JWT && password hash and salt

---
+++ ECommerce API/DTO/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce_API.DTO
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
